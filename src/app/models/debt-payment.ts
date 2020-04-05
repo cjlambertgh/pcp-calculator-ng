b4f93a9313fc71cdca1044e05806b4250c653399
Chris Lambert <phateuk@gmail.com>
2020-04-05 13:47:36 +0100

    add models

---
+++ src/app/models/debt-payment.ts
@@ -0,0 +1,7 @@
+export class DebtPayment {
+    TotalPayment: number;
+    Principal: number;
+    InterestPaid: number;
+    MonthNumber: number;
+    RemainingDebt: number;
+}
---
+++ src/app/models/finance-data.ts
@@ -0,0 +1,8 @@
+export class FinanceData {
+    Price: number;
+    Deposit: number;
+    DealerContribution: number;
+    Term: number;
+    Rate: number;
+    FinalPayment: number;
+}
